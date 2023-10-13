2babe1cedffc6d03b948c308f4be501de564fcb4
John Tharian <jtharian13@gmail.com>
2023-10-13 19:51:04 +0530

    added origins

--- bot_app/src/main.ts
+++ bot_app/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://telegram-weather-bot-roan.vercel.app',
+    origin: [
+      'https://telegram-weather-bot-roan.vercel.app',
+      'telegram-weather-dj0onqezh-johntharians-projects.vercel.app',
+      'https://telegram-weather-bot-johntharians-projects.vercel.app',
+    ],
   });
 
   await app.listen(process.env.PORT || 8000);
