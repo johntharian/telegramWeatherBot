5f3a438dd22ecdbe8dff587f8bdab42f3d0b81ae
John Tharian <jtharian13@gmail.com>
2023-10-13 18:43:44 +0530

    added cors url for prod

--- bot_app/src/main.ts
+++ bot_app/src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:
-      'https://telegram-weather-dj0onqezh-johntharians-projects.vercel.app',
+    origin: 'https://telegram-weather-bot-roan.vercel.ap',
   });
 
   await app.listen(process.env.PORT || 8000);
