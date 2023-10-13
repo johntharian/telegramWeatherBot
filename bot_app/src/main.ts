730e1aef0db5a1145cca6d0e71c915e7a7f62749
John Tharian <jtharian13@gmail.com>
2023-10-13 18:35:58 +0530

    updated server url for prod

--- bot_app/src/main.ts
+++ bot_app/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin:
+      'https://telegram-weather-dj0onqezh-johntharians-projects.vercel.app',
+  });
+
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
